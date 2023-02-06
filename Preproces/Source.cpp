866645bbc1d79145847665fa8b8db0f42bdd3536
Kosmo <The_Kosmo@outlook.com>
2023-02-06 20:50:01 +0500

    fix

--- Preproces/Source.cpp
+++ Preproces/Source.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
 
 #define MONDAY 1
+#define MONDAY_NAME "MONDAY"
+
 #define TUESDAY 2
+#define TUESDAY_NAME "TUESDAY"
+
 #define WEDNESDAY 3
+#define WEDNESDAY_NAME "WEDNESDAY"
+
 #define THORSDAY 4
+#define THORSDAY_NAME "THORSDAY"
+
 #define FRIDAY 5
+#define FRIDAY_NAME "FRIDAY"
+
 #define SATURDAY 6
+#define SATURDAY_NAME "SATURDAY"
+
 #define SUNDAY 7
+#define SUNDAY_NAME "SUNDAY"
+
+#define ENDL << std::endl;
+#define OUT_NAME(a) std::cout << a ENDL;
+#define ERR std::cout << "Invalid date" ENDL;
+
  
 int main()
 {
 	int day = 0;
+	std::cout << "Input number of week: ";
 	std::cin >> day;
 	switch (day)
 	{
-	case MONDAY: std::cout << "Monday\n";
+	case MONDAY: OUT_NAME(MONDAY_NAME)
 		break;
-	case TUESDAY: std::cout << "Tuesday\n";
+	case TUESDAY: OUT_NAME(TUESDAY_NAME)
 		break;
-	case WEDNESDAY: std::cout << "Wednesday\n";
+	case WEDNESDAY: OUT_NAME(WEDNESDAY_NAME)
 		break;
-	case THORSDAY: std::cout << "THORSDAY\n";
+	case THORSDAY: OUT_NAME(THORSDAY_NAME)
 		break;
-	case FRIDAY: std::cout << "FRIDAY\n";
+	case FRIDAY: OUT_NAME(FRIDAY_NAME)
 		break;
-	case SATURDAY: std::cout << "SATURDAY\n";
+	case SATURDAY: OUT_NAME(SATURDAY_NAME)
 		break;
-	case SUNDAY: std::cout << "SUNDAY\n";
+	case SUNDAY: OUT_NAME(SUNDAY_NAME)
+		break;
+	default:
+		ERR
 		break;
 	}
 	
